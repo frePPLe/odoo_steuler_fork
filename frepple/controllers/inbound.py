fab8dca1514e04e01f8efc168f661921ad14590d
Johan De Taeye <jdetaeye@frepple.com>
2024-02-19 14:11:20 +0100

    Export Mo in the status confirmed

--- frepple/controllers/inbound.py
+++ frepple/controllers/inbound.py
@@ -557,7 +557,8 @@ class importer(object):
                             mo._onchange_workorder_ids()
                             mo._onchange_move_raw()
                             mo._create_update_move_finished()
-                            # mo.action_confirm()  # confirm MO
+                            # Steuler: export MO in confirmed status
+                            mo.action_confirm()
                             # mo._plan_workorders() # plan MO
                             # mo.action_assign() # reserve material
                         else:
